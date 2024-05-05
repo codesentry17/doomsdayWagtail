031c12547e5ff301e9f8fcf97bf3225111d7f611
scrappy <scrappycoco902@gmail.com>
2024-05-05 23:01:33 +0530

    get post api for ecg simulation

--- ecg/models.py
+++ ecg/models.py
@@ -3,12 +3,45 @@ from django.shortcuts import render
 
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.fields import StreamField, RichTextField
+from wagtail import blocks
 
 # Create your models here.
 
 
 class EcgPage(Page):
     """This page is for ECG project"""
+
+    classification = StreamField(
+        [
+            ('prediction',blocks.StructBlock(
+                [
+                    ('label',blocks.ChoiceBlock(choices=[('F','F'),('M','M'),('N','N'),('V','V'),('S','S'),('Q','Q')], help_text="Choose Label")),
+                    ('description',blocks.RichTextBlock(features=[ 'bold', 'italic', 'link'], help_text="Some Description"))
+                ]
+            ))
+        ],
+        use_json_field=True,
+        blank=True,
+    )
+
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+
+            ],
+            heading="Form Page Data",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('classification')
+            ],
+            heading="Result Page Data"
+        ),
+    ]
+
 
     def serve(self, request):
         from .forms import UploadECGForm
@@ -20,7 +53,7 @@ class EcgPage(Page):
                 print("Got the image")
 
                 return render(request, 'ecg/ecg_result.html', {
-                    'context':"These are your results"
+                    'page':self
                     }
                 )
             else:
@@ -34,3 +67,4 @@ class EcgPage(Page):
             'form':form
             }
         )
+    
