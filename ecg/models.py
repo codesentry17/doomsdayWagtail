43ee23b038f285f5fecc3e063e5ff55519a742db
scrappy <scrappycoco902@gmail.com>
2024-05-07 04:59:50 +0530

    ecg sort, template remaining

--- ecg/models.py
+++ ecg/models.py
@@ -2,18 +2,24 @@ from django.db import models
 from django.shortcuts import render
 
 
-from wagtail.models import Page
+from wagtail.models import Page, Locale
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.fields import StreamField, RichTextField
 from wagtail import blocks
 
 from .views import predictionFunction
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+
+from pages.models import Blog
 
 # Create your models here.
 
 
 class EcgPage(Page):
     """This page is for ECG project"""
+
+    subpage_types = ['EcgResult']
 
     classification = StreamField(
         [
@@ -24,6 +30,7 @@ class EcgPage(Page):
                 ]
             ))
         ],
+        max_num=6,
         use_json_field=True,
         blank=True,
     )
@@ -32,15 +39,9 @@ class EcgPage(Page):
     content_panels = Page.content_panels + [
         MultiFieldPanel(
             [
-
-            ],
-            heading="Form Page Data",
-        ),
-        MultiFieldPanel(
-            [
                 FieldPanel('classification')
             ],
-            heading="Result Page Data"
+            heading="Form Page Data"
         ),
     ]
 
@@ -61,11 +62,18 @@ class EcgPage(Page):
 
                 predicted_label = predictionFunction(mypic1)
 
-                return render(request, 'ecg/ecg_result.html', {
-                    'page':self,
-                    'predicted_label':predicted_label
-                    }
-                )
+                try:
+                    page = EcgResult.objects.live().get(label=predicted_label, locale=Locale.get_active())
+
+                    return render(request, 'ecg/ecg_result.html', {
+                        'page':page, 
+                        'predicted_label':predicted_label
+                    })
+                    
+                except ObjectDoesNotExist:
+                    return HttpResponse("This page is not yet created by the admin")
+
+                
             else:
                 print("form invalid")
 
@@ -78,3 +86,59 @@ class EcgPage(Page):
             }
         )
     
+
+class EcgResult(Page):
+    """ECG results page, make 6 pages in total"""
+
+    parent_page_types = ['EcgPage']
+
+    label = models.CharField(max_length=1, choices=[
+        ('F','F'),
+        ('M','M'),
+        ('N','N'),
+        ('Q','Q'),
+        ('S','S'),
+        ('V','V')],
+        help_text="This is important") 
+    
+    description = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('label'),
+        FieldPanel('description')
+    ]
+
+
+
+
+# can refer later to 
+# from wagtail.contrib.routable_page.models import RoutablePageMixin, path
+
+# class EcgResult2(RoutablePageMixin, Page):
+#     """This page is meant for displaying the ecg upload results"""
+
+#     parent_page_types = ['EcgPage']
+
+#     label = models.CharField(max_length=1, choices=[
+#         ('F','F'),
+#         ('M','M'),
+#         ('N','N'),
+#         ('Q','Q'),
+#         ('S','S'),
+#         ('V','V')],
+#         help_text="This is important")
+    
+#     discription = RichTextField()
+
+#     content_panels = Page.content_panels + [
+#         FieldPanel('label'),
+#         FieldPanel('discription')
+#     ]
+
+
+
+
+
+
+
+
